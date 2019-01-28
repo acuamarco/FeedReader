49a56c796ebce9eb1932e17a7697ed3e0b459315
Marco Rodriguez <marco@rodriguezcoto.com>
2019-01-28 10:34:24 -0600

    Adding Authentication

---
+++ FeedReader.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FeedReader.Web.Startup))]
+namespace FeedReader.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
