442fa54bad3acc2fa041bf19f296bc7c0eeeebb4
Marco Rodriguez <marco@rodriguezcoto.com>
2019-01-28 21:26:34 -0600

    Adding ArticleService

---
+++ FeedReader.Services/ArticleService.cs
@@ -0,0 +1,28 @@
+﻿using FeedReader.Repository;
+using FeedReader.Repository.Model;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FeedReader.Services
+{
+    public class ArticleService
+    {
+        private FeedReaderContext context;
+
+        public ArticleService(FeedReaderContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<List<Article>> GetArticlesByCategory(int categoryId, int max = 10)
+        {
+            return await context.Articles
+                .Where(a => a.Feed.CategoryId == categoryId)
+                .OrderByDescending(a => a.PublishedDate)
+                .Take(max)
+                .ToListAsync();
+        }
+    }
+}
---
+++ FeedReader.Services/IArticleService.cs
@@ -0,0 +1,11 @@
+﻿using FeedReader.Repository.Model;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace FeedReader.Services
+{
+    public interface IArticleService
+    {
+        Task<List<Article>> GetArticlesByCategory(int categoryId);
+    }
+}
