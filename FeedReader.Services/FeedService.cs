50a4eade44cc9f8a03237f4679b9648582f35913
Marco Rodriguez <marco@rodriguezcoto.com>
2019-01-28 22:23:09 -0600

    Added Feed Service

---
+++ FeedReader.Services/FeedService.cs
@@ -0,0 +1,22 @@
+﻿using System.Data.Entity;
+using System.Threading.Tasks;
+using FeedReader.Repository;
+using FeedReader.Repository.Model;
+
+namespace FeedReader.Services
+{
+    public class FeedService : IFeedService
+    {
+        private FeedReaderContext context;
+
+        public FeedService(FeedReaderContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<Feed> GetByIdWithCategory(int feedId)
+        {
+            return await context.Feeds.Include(f => f.Category).SingleAsync(f => f.Id == feedId);
+        }
+    }
+}
---
+++ FeedReader.Services/IFeedService.cs
@@ -0,0 +1,10 @@
+﻿using FeedReader.Repository.Model;
+using System.Threading.Tasks;
+
+namespace FeedReader.Services
+{
+    public interface IFeedService
+    {
+        Task<Feed> GetByIdWithCategory(int feedId);
+    }
+}
