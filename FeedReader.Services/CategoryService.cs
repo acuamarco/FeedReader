50d1d425f48f6aa99e96a9796a01465fb1e5f812
Marco Rodriguez <marco@rodriguezcoto.com>
2019-01-28 21:26:51 -0600

    Adding CategoryService

---
+++ FeedReader.Services/CategoryService.cs
@@ -0,0 +1,22 @@
+﻿using System.Data.Entity;
+using System.Threading.Tasks;
+using FeedReader.Repository;
+using FeedReader.Repository.Model;
+
+namespace FeedReader.Services
+{
+    public class CategoryService : ICategoryService
+    {
+        private FeedReaderContext context;
+
+        public CategoryService(FeedReaderContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<Category> GetByIdWithFeeds(int categoryId)
+        {
+            return await context.Categories.Include(c => c.Feeds).SingleAsync(c => c.Id == categoryId);
+        }
+    }
+}
---
+++ FeedReader.Services/ICategoryService.cs
@@ -0,0 +1,14 @@
+﻿using FeedReader.Repository.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FeedReader.Services
+{
+    public interface ICategoryService
+    {
+        Task<Category> GetByIdWithFeeds(int id);
+    }
+}
